9971e8f0355a389202f322384a91da59813924b4
kamelot43 <andrepud@mail.ru>
2018-03-28 19:08:01 +0300

    Внесены изменения в имена переменных

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const rightArrow = 39;
 const template = document.getElementById(`templates`);
 const screens = template.content.children;
 const sectionMain = document.querySelector(`.main`);
-let start = 0;
+let startScreenIndex = 0;
 
 // Создание функции отрисовки заданного экрана
 
@@ -14,23 +14,26 @@ let renderScreen = (num) => {
   sectionMain.appendChild(node);
 };
 
-renderScreen(0);
+renderScreen(startScreenIndex);
 
 
 // Переключение экрана. Обработчик события.
 
 document.addEventListener(`keydown`, function (evt) {
 
-  if (evt.altKey && evt.keyCode === rightArrow) {
-    if (start < screens.length - 1) {
-      start += 1;
-      renderScreen(start);
+  if (!evt.altKey) {
+    return;
+  } else if (evt.keyCode === rightArrow) {
+    if (startScreenIndex < screens.length - 1) {
+      startScreenIndex += 1;
+      renderScreen(startScreenIndex);
     }
-  } else if (evt.altKey && evt.keyCode === leftArrow) {
-    if (start > 0) {
-      start -= 1;
-      renderScreen(start);
+  } else if (evt.keyCode === leftArrow) {
+    if (startScreenIndex > 0) {
+      startScreenIndex -= 1;
+      renderScreen(startScreenIndex);
     }
   }
+
 
 });
