eaafe581906bec1a1b4a62969e775f94c8167164
kamelot43 <andrepud@mail.ru>
2018-03-29 19:00:33 +0300

    лишние пробелы убраны

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-'use sctrict';
 const leftArrow = 37;
 const rightArrow = 39;
 const template = document.getElementById(`templates`);
@@ -7,27 +6,21 @@ const sectionMain = document.querySelector(`.main`);
 let startScreenIndex = 0;
 
 // Создание функции отрисовки заданного экрана
-
-let renderScreen = (num) => {
-  let node = screens[num].cloneNode(true);
+const renderScreen = (num) => {
+  const node = screens[num].cloneNode(true);
   sectionMain.innerHTML = ``;
   sectionMain.appendChild(node);
 };
-
 renderScreen(startScreenIndex);
 
-
 // Переключение экрана. Обработчик события.
-
 document.addEventListener(`keydown`, function (evt) {
-
   if (!evt.altKey) {
     return;
   }
   if (evt.keyCode === rightArrow && startScreenIndex < screens.length - 1) {
     startScreenIndex += 1;
     renderScreen(startScreenIndex);
-
   } else if (evt.keyCode === leftArrow && startScreenIndex > 0) {
     startScreenIndex -= 1;
     renderScreen(startScreenIndex);
