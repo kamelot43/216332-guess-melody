592daf824855efff040a201284e99203c012f779
kamelot43 <andrepud@mail.ru>
2018-04-08 23:01:19 +0300

    реализованы 2 функции из 3. Частично покрыты тестами

---
+++ js/data/calc-points.js
@@ -0,0 +1,33 @@
+// Переменные для тестирования
+// Начальные значения игры
+export const INITIAL_GAME = {
+  life: 3,
+  quickAnswer: 30,
+  success: 10,
+  point: 1,
+  doublePoints: 2
+};
+
+export const calcPoints = (arr, note) => {
+
+  const failAnswers = arr.filter(function (current) {
+    return !current.success;
+  });
+
+  if (arr.length !== INITIAL_GAME.success) {
+    return -1;
+  } else if (failAnswers.length > note) {
+    return -1;
+  }
+
+  return arr.reduce(function (sum, current) {
+    if (current.success && current.time < INITIAL_GAME.quickAnswer) {
+      return sum + INITIAL_GAME.doublePoints;
+    } else if (current.success) {
+      return sum + INITIAL_GAME.point;
+    } else if (!current.success) {
+      return sum - INITIAL_GAME.doublePoints;
+    }
+    return sum;
+  }, 0);
+};
